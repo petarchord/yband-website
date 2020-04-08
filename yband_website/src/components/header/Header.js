0dc2aa8b433e934714e083e28a086a0865c459f8
petarchord <petarchord@hotmail.com>
2020-04-08 20:39:09 +0200

    initial commit

---
+++ yband_website/src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <img alt="logo" className="logo"></img>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
