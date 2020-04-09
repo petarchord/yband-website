e25b977d7ace4d34ac48c1aa9d589e56943fd049
petarchord <petarchord@hotmail.com>
2020-04-09 19:26:05 +0200

    navigation in progress

--- yband_website/src/components/header/Header.js
+++ yband_website/src/components/header/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../assets/logo_white.png";
+import fbLogo from "../../assets/fb_icon.png";
+import instaLogo from "../../assets/insta_icon.png";
+import ytLogo from "../../assets/yt_icon.png";
 
 const Header = () => {
   return (
     <header>
-      <div className="header">
-        <img alt="logo" className="logo"></img>
+      <div className="container">
+        <div className="img_holder">
+          <a href="#">
+            <img alt="logo" src={logo} className="logo"></img>
+          </a>
+        </div>
+
         <nav>
           <ul>
             <li>
@@ -22,6 +31,25 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+        <div className="social_networks">
+          <ul>
+            <li>
+              <a href="#">
+                <img src={fbLogo} className="fb"></img>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={instaLogo} className="insta"></img>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={ytLogo} className="yt"></img>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
