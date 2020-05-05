cac9cb7e34ea09c3a25b672a72882237b097e627
petarchord <petarchord@hotmail.com>
2020-05-05 14:49:34 +0200

    accordion edited

--- yband_website/src/components/Term/Accordion.jsx
+++ yband_website/src/components/Term/Accordion.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import styles from "./Accordion.module.css";
 const Accordion = () => {
   const onClick = e => {
-    console.log(e.target);
-    e.target.classList.toggle("active");
-    console.log(e.target.classList);
-    let panel = e.target.nextElementSibling;
+    let button;
+    if (e.target.nodeName === "BUTTON") button = e.target;
+    else button = e.target.parentNode;
+    let panel = button.nextElementSibling;
     if (panel.style.maxHeight) {
+      button.firstChild.innerHTML = "+";
       panel.style.maxHeight = null;
     } else {
+      button.firstChild.innerHTML = "-";
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   };
@@ -18,23 +20,86 @@ const Accordion = () => {
     <div className={styles.container}>
       <div className={styles.accordion_holder}>
         <button onClick={e => onClick(e)} className={styles.accordion}>
-          Januar
+          <span className={styles.sign}>+</span> Januar
         </button>
         <div className={styles.panel}>
           <p>01. Restoran Diamant Nis</p>
         </div>
 
         <button onClick={e => onClick(e)} className={styles.accordion}>
-          Februar
+          <span className={styles.sign}>+</span> Februar
         </button>
         <div className={styles.panel}>
           <p>02. Restordan Diamant Nis</p>
         </div>
         <button onClick={e => onClick(e)} className={styles.accordion}>
-          Mart
+          <span className={styles.sign}>+</span> Mart
         </button>
         <div className={styles.panel}>
           <p>02. Restordan Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> April
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Maj
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Jun
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Jul
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Avgust
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Septembar
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Oktobar
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Novembar
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
+        </div>
+
+        <button onClick={e => onClick(e)} className={styles.accordion}>
+          <span className={styles.sign}>+</span> Decembar
+        </button>
+        <div className={styles.panel}>
+          <p>01. Restoran Diamant Nis</p>
         </div>
       </div>
     </div>
