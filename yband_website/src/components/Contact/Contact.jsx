b7f30384be2427aba5b15451bc1df6c3658c6a07
petarchord <petarchord@hotmail.com>
2020-05-07 13:49:54 +0200

    contact layout done

--- yband_website/src/components/Contact/Contact.jsx
+++ yband_website/src/components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import fbLogo from "../../assets/fb_icon.png";
+import instaLogo from "../../assets/insta_icon.png";
+import ytLogo from "../../assets/yt_icon.png";
 const Contact = () => {
   return (
     <div className={styles.container}>
@@ -12,10 +15,15 @@ const Contact = () => {
       <div className={styles.contact_holder}>
         <div className={styles.text_holder}>
           <h5>Kontaktirajte nas</h5>
-          <p>
-            Pozovite <span className={styles.number}>(+381)64/54-77-689</span>
-            ili <span className={styles.number}>(+381)61/80-87-062</span>
-          </p>
+          <div>
+            <img src="https://img.icons8.com/ios/50/000000/phone.png" />
+            <p>(+381)64/54-77-689</p>
+            <p>(+381)61/80-87-062</p>
+            <div className={styles.email_wrapper}>
+              <img src="https://img.icons8.com/ios/50/000000/important-mail.png" />
+              <p>Pošaljite nam poruku i dobićete odgovor u najkraćem roku.</p>
+            </div>
+          </div>
         </div>
         <div className={styles.form_holder}>
           <form>
@@ -33,8 +41,16 @@ const Contact = () => {
               <label htmlFor="message">Vaša poruka</label>
               <textarea name="message" id="msg" cols="30" rows="10"></textarea>
             </div>
-            <input type="submit" id={styles.submit} value="Pošaljite poruku" />
+            <input type="submit" id={styles.submit} value="POŠALJITE PORUKU" />
           </form>
+        </div>
+        <div className={styles.social_network}>
+          <h5>Pratite nas na društvenim mrežama</h5>
+          <div className={styles.network_holder}>
+            <img src={fbLogo} id={styles.fb} alt="" />
+            <img src={instaLogo} id={styles.insta} alt="" />
+            <img src={ytLogo} id={styles.yt} alt="" />
+          </div>
         </div>
       </div>
     </div>
